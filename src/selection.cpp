b99c23b71afbfdeb34d4d9e9821029b962725503
fluteur <malo.leroy@student-cs.fr>
2025-01-16 15:27:30 +0100

    ✅ Fix selection tests

--- src/selection.cpp
+++ src/selection.cpp
@@ -1,10 +1,42 @@
 #include <random>
 #include <algorithm>
-#include <iostream>
 #include <selection.h>
+
+void getRandomMergees(std::vector<Diagram *> candidates, selection::mergees *m)
+{
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(candidates.begin(), candidates.end(), g);
+    m->a = candidates[0];
+    m->b = candidates[1];
+}
+
+void getMergeesWithMaxAmplitude(std::vector<Diagram *> candidates, selection::mergees *m)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+void getMergeesWithMinAmplitude(std::vector<Diagram *> candidates, selection::mergees *m)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+void getMergeesWithMaxNodes(std::vector<Diagram *> candidates, selection::mergees *m)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+void getMergeesWithMinNodes(std::vector<Diagram *> candidates, selection::mergees *m)
+{
+    throw std::runtime_error("Not implemented");
+}
 
 selection::mergees selection::getMergeesAtHeight(const size_t h, Diagram &d, MergeesChoiceStrategy strategy)
 {
+    if (h == 0)
+    {
+        throw std::invalid_argument("0-height diagrams have no mergees");
+    }
     struct mergees result;
     std::vector<Diagram *> candidates = d.getNodePointersAtHeight(h);
     switch (strategy)
@@ -35,39 +67,5 @@ selection::mergees selection::getMergeesAtHeight(const size_t h, Diagram &d, Mer
         break;
     }
     }
-    throw std::runtime_error("Unreachable code");
-}
-
-selection::mergees selection::getMergeesAtHeight(const size_t h, Diagram &d, MergeesChoiceStrategy strategy)
-{
-    throw std::runtime_error("0-height diagrams has no mergees");
-}
-
-void getRandomMergees(std::vector<Diagram *> candidates, selection::mergees *m)
-{
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(candidates.begin(), candidates.end(), g);
-    m->a = candidates[0];
-    m->b = candidates[1];
-}
-
-void getMergeesWithMaxAmplitude(std::vector<Diagram *> candidates, selection::mergees *m)
-{
-    throw std::runtime_error("Not implemented");
-}
-
-void getMergeesWithMinAmplitude(std::vector<Diagram *> candidates, selection::mergees *m)
-{
-    throw std::runtime_error("Not implemented");
-}
-
-void getMergeesWithMaxNodes(std::vector<Diagram *> candidates, selection::mergees *m)
-{
-    throw std::runtime_error("Not implemented");
-}
-
-void getMergeesWithMinNodes(std::vector<Diagram *> candidates, selection::mergees *m)
-{
-    throw std::runtime_error("Not implemented");
+    return result;
 }
