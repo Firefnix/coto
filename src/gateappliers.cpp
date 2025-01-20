4388b587aa679930cb0180d66ffa955d1860d015
fluteur <malo.leroy@student-cs.fr>
2025-01-20 16:21:39 +0100

    ✨ Implement X gate applier

--- src/gateappliers.cpp
+++ src/gateappliers.cpp
@@ -1,38 +1,47 @@
 #include <gateappliers.h>
 
-
-void gateappliers::applyX(Diagram *d, qubit q)
+static void assertQubitIsValid(Diagram *d, qubit q)
 {
     if (d->height < q)
     {
-        throw std::runtime_error("Invalid qubit");
+        throw std::runtime_error("Invalid qubit: Trying to apply qubit " + std::to_string(q) + " to a diagram of height " + std::to_string(d->height));
     }
-    throw std::runtime_error("Not implemented");
+}
+
+void gateappliers::applyX(Diagram *d, qubit q)
+{
+    assertQubitIsValid(d, q);
+    if (q == 0)
+    {
+        std::swap(d->left, d->right);
+        return;
+    }
+    for (auto &g : d->left)
+    {
+        applyX(g.d, q - 1);
+    }
+    for (auto &g : d->right)
+    {
+        applyX(g.d, q - 1);
+    }
 }
 
 void gateappliers::applyH(Diagram *d, qubit q)
 {
-    if (d->height < q)
-    {
-        throw std::runtime_error("Invalid qubit");
-    }
+    assertQubitIsValid(d, q);
     throw std::runtime_error("Not implemented");
 }
 
 void gateappliers::applyS(Diagram *d, qubit a, qubit b)
 {
-    if (d->height < a || d->height < b)
-    {
-        throw std::runtime_error("Invalid qubit");
-    }
+    assertQubitIsValid(d, a);
+    assertQubitIsValid(d, b);
     throw std::runtime_error("Not implemented");
 }
 
 void gateappliers::applyCX(Diagram *d, qubit a, qubit b)
 {
-    if (d->height < a || d->height < b)
-    {
-        throw std::runtime_error("Invalid qubit");
-    }
+    assertQubitIsValid(d, a);
+    assertQubitIsValid(d, b);
     throw std::runtime_error("Not implemented");
 }
