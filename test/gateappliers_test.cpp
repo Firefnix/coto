62c8ecb749e4f511125d3f9c0833d7ad14ee3691
fluteur <malo.leroy@student-cs.fr>
2025-02-13 01:44:39 +0100

    ✅ Test phase gate

--- test/gateappliers_test.cpp
+++ test/gateappliers_test.cpp
@@ -47,3 +47,43 @@ TEST_F(GateAppliersTest, X)
         }
     }
 }
+
+TEST_F(GateAppliersTest, phase)
+{
+    for (auto numberOfQubits = 1; numberOfQubits < 9; numberOfQubits++)
+    {
+        for (auto qubitToApplyPhaseOn = 0; qubitToApplyPhaseOn < numberOfQubits; qubitToApplyPhaseOn++)
+        {
+            const auto phaseDenominator = 3;
+            ampl::ConcreteState base(numberOfQubits);
+            for (auto i = 0; i < base.size(); i++)
+            {
+                base[i] = i;
+            }
+            Evaluation expected(numberOfQubits);
+            for (auto i = 0; i < expected.size(); i++)
+            {
+                expected[i] = !(i & (1 << (numberOfQubits - qubitToApplyPhaseOn - 1)))
+                                  ? absi::Interval::real(i)
+                                  : polar::Interval(polar::PositiveInterval(i), polar::AngleInterval(2. / phaseDenominator, 0));
+                std::cout << expected[i].to_string() << std::endl;
+            }
+
+            auto d = Diagram::fromStateVector(base);
+            gateappliers::applyPhase(d, qubitToApplyPhaseOn, phaseDenominator);
+
+            auto ev = d->evaluate();
+            std::cout << "At height " << qubitToApplyPhaseOn << ", got " << d->countNodesAtHeight(qubitToApplyPhaseOn) << std::endl;
+            for (auto i = 0; i < expected.size(); i++)
+            {
+                EXPECT_EQ(ev[i], expected[i])
+                    << "Failed appplying phase(2*pi/" << phaseDenominator
+                    << ") on qubit " << qubitToApplyPhaseOn
+                    << " at index " << i
+                    << " got result " << ev[i].to_string()
+                    << ", while expected " << expected[i].to_string();
+            }
+            std::cout << "--------" << std::endl;
+        }
+    }
+}
