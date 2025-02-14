96ee9062f8924c8ac6b7b6238134e95b86b273bc
fluteur <malo.leroy@student-cs.fr>
2025-02-14 20:58:06 +0100

    🧪 Add tests for a general gate applier

--- test/gateappliers_test.cpp
+++ test/gateappliers_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <gateappliers.h>
 
+const size_t MAX_QUBITS = 8;
+
 size_t flipNthBit(const size_t n, const size_t index)
 {
     return index ^ (1 << n);
@@ -17,7 +19,7 @@ public:
 
 TEST_F(GateAppliersTest, X)
 {
-    for (auto numberOfQubits = 1; numberOfQubits < 9; numberOfQubits++)
+    for (auto numberOfQubits = 1; numberOfQubits < MAX_QUBITS; numberOfQubits++)
     {
         for (auto qubitToApplyXOn = 0; qubitToApplyXOn < numberOfQubits; qubitToApplyXOn++)
         {
@@ -29,7 +31,7 @@ TEST_F(GateAppliersTest, X)
             Evaluation result(numberOfQubits);
             for (auto i = 0; i < result.size(); i++)
             {
-                result[flipNthBit(numberOfQubits - qubitToApplyXOn - 1, i)] = absi::Interval::real(i);
+                result[flipNthBit(numberOfQubits - qubitToApplyXOn - 1, i)] = Interval::real(i);
             }
 
             auto d = Diagram::fromStateVector(base);
@@ -50,7 +52,7 @@ TEST_F(GateAppliersTest, X)
 
 TEST_F(GateAppliersTest, phase)
 {
-    for (auto numberOfQubits = 1; numberOfQubits < 9; numberOfQubits++)
+    for (auto numberOfQubits = 1; numberOfQubits < MAX_QUBITS; numberOfQubits++)
     {
         for (auto qubitToApplyPhaseOn = 0; qubitToApplyPhaseOn < numberOfQubits; qubitToApplyPhaseOn++)
         {
@@ -85,5 +87,84 @@ TEST_F(GateAppliersTest, phase)
             }
             std::cout << "--------" << std::endl;
         }
+    }
+}
+
+TEST_F(GateAppliersTest, gateMatrixIdentity)
+{
+    for (auto numberOfQubits = 1; numberOfQubits < MAX_QUBITS; numberOfQubits++)
+    {
+        for (auto qubitToApplyIdentityOn = 0; qubitToApplyIdentityOn < numberOfQubits; qubitToApplyIdentityOn++)
+        {
+            gateappliers::GateMatrix id(1);
+            id(0, 0) = Interval::real(1);
+            id(1, 1) = Interval::real(1);
+
+            ampl::ConcreteState base(numberOfQubits);
+            for (auto i = 0; i < base.size(); i++)
+            {
+                base[i] = i;
+            }
+
+            auto d = Diagram::fromStateVector(base);
+            gateappliers::applyGateMatrix(d, qubitToApplyIdentityOn, id);
+
+            auto ev = d->evaluate();
+            for (auto i = 0; i < base.size(); i++)
+            {
+                EXPECT_EQ(ev[i], Interval::singleton(base[i]))
+                    << "Failed appplying identity on qubit 0"
+                    << " at index " << i
+                    << " got " << ev[i].to_string()
+                    << ", expected " << base[i];
+            }
+        }
+    }
+}
+
+TEST_F(GateAppliersTest, gateMatrixHadamardOnQubit0)
+{
+    auto m = gateappliers::GateMatrix(1);
+    m(0, 0) = 1;
+    m(0, 1) = 1;
+    m(1, 0) = 1;
+    m(1, 1) = -1;
+
+    ampl::Amplitude v[] = {1, 2, 3, 4};
+    ampl::ConcreteState base(2, v);
+
+    auto d = Diagram::fromStateVector(base);
+
+    gateappliers::applyGateMatrix(d, 0, m);
+
+    auto ev = d->evaluate();
+    EXPECT_EQ(ev[0], Interval::real(4)) << ev[0].to_string() << " != 4";
+    EXPECT_EQ(ev[1], Interval::real(6)) << ev[1].to_string() << " != 6";
+    EXPECT_EQ(ev[2], Interval::real(-2)) << ev[2].to_string() << " != -2";
+    EXPECT_EQ(ev[3], Interval::real(-2)) << ev[3].to_string() << " != -2";
+}
+
+TEST_F(GateAppliersTest, gateMatrixHadamardOnQubit1)
+{
+    absi::Interval v[] = {1, 1, 1, -1};
+    gateappliers::GateMatrix m(1, v);
+
+    ampl::ConcreteState base(3);
+    for (auto i = 0; i < base.size(); i++)
+    {
+        base[i] = i + 1;
+    }
+
+    auto d = Diagram::fromStateVector(base);
+    gateappliers::applyGateMatrix(d, 1, m);
+    auto computed = d->evaluate();
+
+    ampl::Amplitude expected[8] = {4, 6, -2, -2, 12, 14, -2, -2};
+    for (auto i = 0; i < 8; i++)
+    {
+        EXPECT_EQ(computed[i], Interval::singleton(expected[i]))
+            << "Failed applying H on qubit 1 at index " << i
+            << ", computed " << computed[i].to_string()
+            << ", expected " << expected[i];
     }
 }
