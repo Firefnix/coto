98cef021199d1a13257c5e46c8911353ca934649
fluteur <malo.leroy@student-cs.fr>
2025-02-14 20:53:15 +0100

    ✨ Add PowMatrix

---
+++ include/powmatrix.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <memory>
+
+template <typename T>
+class PowMatrix
+{
+private:
+    /// @brief The height of the array (n)
+    size_t height_;
+
+    /// @brief The size of the array (2^n)
+    size_t size_;
+
+    /// @brief The size of the square array (2^n * 2^n)
+    size_t square_size_;
+
+    /// @brief Array data
+    std::unique_ptr<T[]> data_;
+
+public:
+    /// @brief Constructor: initializes the matrix with size 2^n
+    explicit PowMatrix(const size_t n);
+
+    explicit PowMatrix(const size_t n, const T *value);
+
+    /// @brief Access element
+    T &operator()(size_t iindex, size_t jindex);
+
+    const T &operator()(size_t iindex, size_t jindex) const;
+
+    /// @brief Access element at row 0 and column 0 if the height is 0, otherwise throws an exception
+    T value() const;
+
+    /// @brief The matrix is of "size" 2^n x 2^n, here n is the size of the matrix
+    /// @return The size of the matrix
+    inline size_t height() const noexcept
+    {
+        return height_;
+    }
+
+    /// @brief Get size of the array
+    inline size_t size() const noexcept
+    {
+        return size_;
+    }
+
+    PowMatrix<T> topLeft() const;
+
+    PowMatrix<T> topRight() const;
+
+    PowMatrix<T> bottomLeft() const;
+
+    PowMatrix<T> bottomRight() const;
+};
---
+++ src/powmatrix.cpp
@@ -0,0 +1,123 @@
+#include "powmatrix.h"
+
+template <typename T>
+PowMatrix<T>::PowMatrix(const size_t n)
+{
+    if (n + 1 >= sizeof(size_t) * 8)
+    {
+        throw std::overflow_error("Exponent too large"); // Prevent overflow
+    }
+    height_ = n;
+    size_ = static_cast<size_t>(1) << n; // Compute 2^n
+    square_size_ = size_ * size_;
+    data_ = std::make_unique<T[]>(square_size_);
+}
+
+template <typename T>
+PowMatrix<T>::PowMatrix(size_t n, const T *value)
+{
+    if (n + 1 >= sizeof(size_t) * 8)
+    {
+        throw std::overflow_error("Exponent too large"); // Prevent overflow
+    }
+    height_ = n;
+    size_ = static_cast<size_t>(1) << n; // Compute 2^n
+    square_size_ = size_ * size_;
+    data_ = std::make_unique<T[]>(square_size_);
+    for (size_t i = 0; i < square_size_; i++)
+    {
+        data_[i] = value[i];
+    }
+}
+
+template <typename T>
+T &PowMatrix<T>::operator()(size_t iindex, size_t jindex)
+{
+    if (iindex >= size_ || jindex >= size_)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[iindex * size_ + jindex];
+}
+
+template <typename T>
+const T &PowMatrix<T>::operator()(size_t iindex, size_t jindex) const
+{
+    if (iindex >= size_ || jindex >= size_)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[iindex * size_ + jindex];
+}
+
+template <typename T>
+T PowMatrix<T>::value() const
+{
+    if (height_ != 0)
+    {
+        throw std::runtime_error("Matrix is not a scalar");
+    }
+    return data_[0];
+}
+
+template <typename T>
+PowMatrix<T> PowMatrix<T>::topLeft() const
+{
+    auto part = PowMatrix(height() - 1);
+    for (size_t i = 0; i < part.size(); i++)
+    {
+        for (size_t j = 0; j < part.size(); j++)
+        {
+            part(i, j) = (*this)(i, j);
+        }
+    }
+    return part;
+}
+
+template <typename T>
+PowMatrix<T> PowMatrix<T>::topRight() const
+{
+    auto part = PowMatrix(height() - 1);
+    for (size_t i = 0; i < part.size(); i++)
+    {
+        for (size_t j = 0; j < part.size(); j++)
+        {
+            part(i, j) = (*this)(i + part.size(), j);
+        }
+    }
+    return part;
+}
+
+template <typename T>
+PowMatrix<T> PowMatrix<T>::bottomLeft() const
+{
+    auto part = PowMatrix(height() - 1);
+    for (size_t i = 0; i < part.size(); i++)
+    {
+        for (size_t j = 0; j < part.size(); j++)
+        {
+            part(i, j) = (*this)(i, j + part.size());
+        }
+    }
+    return part;
+}
+
+template <typename T>
+PowMatrix<T> PowMatrix<T>::bottomRight() const
+{
+    auto part = PowMatrix(height() - 1);
+    for (size_t i = 0; i < part.size(); i++)
+    {
+        for (size_t j = 0; j < part.size(); j++)
+        {
+            part(i, j) = (*this)(i + part.size(), j + part.size());
+        }
+    }
+    return part;
+}
+
+#include "amplitude.h"
+#include "absi.h"
+
+template class PowMatrix<ampl::Amplitude>;
+template class PowMatrix<absi::Interval>;
