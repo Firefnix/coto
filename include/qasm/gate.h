df38bcd6fab35ec8460898410e9b594e95407239
fluteur <malo.leroy@student-cs.fr>
2025-02-27 19:22:51 +0100

    🎨 Reformat gate.hh

--- include/qasm/gate.h
+++ include/qasm/gate.h
@@ -8,12 +8,13 @@
 #include <vector>
 
 /// @brief A quantum gate
-class Gate {
+class Gate
+{
 public:
     [[nodiscard]] static bool exists(const std::string &gateName) noexcept;
     [[nodiscard]] static const Gate byName(const std::string &gateName);
-    void applyTo(const std::vector<varname>& qubitsNames) const;
-    virtual void applyTo(const std::vector<qubit>& qubits) const;
+    void applyTo(const std::vector<varname> &qubitsNames) const;
+    virtual void applyTo(const std::vector<qubit> &qubits) const;
 
     /// @brief The number of qubits of the gate
     std::size_t size() const noexcept;
@@ -21,20 +22,23 @@ public:
     std::string toString() const noexcept;
 
     const std::string name;
+
 protected:
-    Gate(const std::string& name, const std::size_t size);
+    Gate(const std::string &name, const std::size_t size);
     const std::size_t gateSize;
 };
 
 /// @brief A phase gate
-class PhaseGate : public Gate {
+class PhaseGate : public Gate
+{
 public:
-    static bool is(const std::string& gateName);
-    PhaseGate(const std::string& gateName);
+    [[nodiscard]] static bool is(const std::string &gateName);
+    PhaseGate(const std::string &gateName);
 
     const int phase;
+
 protected:
     PhaseGate(int phase);
 };
 
-std::string gateToString(const std::string& name);
+std::string gateToString(const std::string &name);
